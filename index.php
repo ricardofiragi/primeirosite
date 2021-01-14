b8d7c5e576fcf565d59128b968d62c3fc0eae888
ricardofiragi <ricardo.firagi@gmail.com>
2021-01-14 14:47:43 -0300

    nova formatcao em negrito

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
-Esse é o primeiro site
-
-<p>Example of <b>1 - primeiro fizemos a conexão do código no github</b></p>
-
-<p>Example of <b>2 - Teste para acrescentar novo código, fazer o push no git push e o deploy automático no heroku</b></p>
+git add .<p>o nome do site <b>primeiro site está em negrito</b></p
