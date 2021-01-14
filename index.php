72375a59872b1f63954cadf7a75a283c653dd2e0
ricardofiragi <ricardo.firagi@gmail.com>
2021-01-14 15:02:05 -0300

    nova formatacao

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-<p>o nome do site <b>Primeiro Site</b> está em negrito</p
+<p>o nome do site <b>Primeiro Site</b> está em negrito</p>
+
+<a href="https://api.whatsapp.com/send?phone=5519996795545">clique aqui para me acionar pelo whatsapp</a>
