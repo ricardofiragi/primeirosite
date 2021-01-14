7ad688543f1399f3db7d7273c1e0e3777e0729af
ricardofiragi <ricardo.firagi@gmail.com>
2021-01-14 14:44:56 -0300

    atualização de codigo formatacao

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 Esse é o primeiro site
-1 - primeiro fizemos a conexão do código no github
+
+<p>Example of <b>1 - primeiro fizemos a conexão do código no github</b></p>
+
+<p>Example of <b>2 - Teste para acrescentar novo código, fazer o push no git push e o deploy automático no heroku</b></p>
